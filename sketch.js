81b838ce12b2b0e73c84b271ac76b0e11b63a5de
fangruiyu <150638449+fangruiyu@users.noreply.github.com>
2024-06-03 11:57:53 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,11 @@
-/* MoveNet Skeleton - Steve's Makerspace (most of this code is from TensorFlow)
-
-MoveNet is developed by TensorFlow:
-https://www.tensorflow.org/hub/tutorials/movenet
-
-*/
-
 let video, bodypose, pose, keypoint, detector;
 let poses = [];
 
-var Img
+var Img;
 function preload(){
-	Img= loadImage("upload_7dd6374659c38a191c0e3eb86f1d75c5.gif")
+  Img = loadImage("upload_7dd6374659c38a191c0e3eb86f1d75c5.gif");
 }
+
 async function init() {
   const detectorConfig = {
     modelType: poseDetection.movenet.modelType.MULTIPOSE_LIGHTNING,
@@ -31,7 +25,6 @@ async function getPoses() {
   if (detector) {
     poses = await detector.estimatePoses(video.elt, {
       maxPoses: 2,
-      //flipHorizontal: true,
     });
   }
   requestAnimationFrame(getPoses);
@@ -73,50 +66,44 @@ function drawSkeleton() {
       pop();
     }
     // shoulder to wrist
-    for (j = 5; j < 9; j++) {
+    for (let j = 5; j < 9; j++) {
       if (pose.keypoints[j].score > 0.1 && pose.keypoints[j + 2].score > 0.1) {
-        partA = pose.keypoints[j];
-        partB = pose.keypoints[j + 2];
+        let partA = pose.keypoints[j];
+        let partB = pose.keypoints[j + 2];
         line(partA.x, partA.y, partB.x, partB.y);
       }
     }
     // shoulder to shoulder
-    partA = pose.keypoints[5];
-    partB = pose.keypoints[6];
+    let partA = pose.keypoints[5];
+    let partB = pose.keypoints[6];
     if (partA.score > 0.1 && partB.score > 0.1) {
       line(partA.x, partA.y, partB.x, partB.y);
-      
     }
     // hip to hip
     partA = pose.keypoints[11];
     partB = pose.keypoints[12];
     if (partA.score > 0.1 && partB.score > 0.1) {
       line(partA.x, partA.y, partB.x, partB.y);
-      
     }
     // shoulders to hips
     partA = pose.keypoints[5];
     partB = pose.keypoints[11];
     if (partA.score > 0.1 && partB.score > 0.1) {
       line(partA.x, partA.y, partB.x, partB.y);
-      
     }
     partA = pose.keypoints[6];
     partB = pose.keypoints[12];
     if (partA.score > 0.1 && partB.score > 0.1) {
       line(partA.x, partA.y, partB.x, partB.y);
-      
     }
     // hip to foot
-    for (j = 11; j < 15; j++) {
+    for (let j = 11; j < 15; j++) {
       if (pose.keypoints[j].score > 0.1 && pose.keypoints[j + 2].score > 0.1) {
         partA = pose.keypoints[j];
         partB = pose.keypoints[j + 2];
         line(partA.x, partA.y, partB.x, partB.y);
-        
       }
     }
-
   }
 }
 
@@ -134,7 +121,7 @@ function drawSkeleton() {
   10 right wrist
   11 left hip
   12 right hip
-  13 left kneee
+  13 left knee
   14 right knee
   15 left foot
   16 right foot
